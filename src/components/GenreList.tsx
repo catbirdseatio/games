54db863635c68c5d3eb622d5636f3433a0ceaac0
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-03-21 18:53:00 -0400

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,32 @@
-import { list } from "@chakra-ui/react";
+import {
+  List,
+  ListIcon,
+  HStack,
+  Image,
+  Text,
+  ListItem,
+} from "@chakra-ui/react";
 import React from "react";
 import useGenres, { Genre } from "../hooks/useGenres";
-import useData from "../hooks/useData";
+import getCroppedImageUrl from "../services/imageUrl";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
-      {data.map((genre: Genre) => (
-        <li key={genre.id}>{genre.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY={"0.3125rem"}>
+          <HStack>
+            <Image
+              boxSize={"2rem"}
+              src={getCroppedImageUrl(genre.image_background)}
+              borderRadius={8}
+            />
+            <Text fontSize={"lg"}>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
