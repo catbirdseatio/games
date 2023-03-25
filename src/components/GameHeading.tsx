e096705eacd8886bdc71f76f5653552f6014be72
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-03-25 02:36:34 -0400

    Add a dynamic page headig

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,20 @@
+import { Heading } from "@chakra-ui/react";
+import { GameQuery } from "../App";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+const GameHeading = ({ gameQuery }: Props) => {
+  console.log(gameQuery);
+  const heading = `${gameQuery.platform?.name || ""} ${
+    gameQuery.genre?.name || ""
+  } Games`;
+  return (
+    <Heading as="h1" marginY={5} marginX={9} fontSize={"5xl"}>
+      {heading}
+    </Heading>
+  );
+};
+
+export default GameHeading;
